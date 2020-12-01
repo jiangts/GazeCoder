51e6c303fb73ca233cc018b8c4e70075f104d4e5
Allan Jiang <origami789@gmail.com>
2020-12-01 10:36:55 -0800

    create logs

--- ws.js
+++ ws.js
@@ -1,4 +1,23 @@
 var _ = require('lodash')
+const winston = require('winston');
+const path = require('path');
+const {transports, createLogger, format} = require('winston');
+
+const makeLogger = filename => {
+  filename = path.join(__dirname, 'public/logs', filename)
+  const logger = createLogger({
+    format: format.combine(
+      format.timestamp(),
+      format.json()
+    ),
+    transports: [
+      new transports.File({filename})
+    ]
+  });
+  return logger
+}
+
+const loggers = {}
 
 module.exports = (io, app) => {
   app.io = io
@@ -13,11 +32,15 @@ module.exports = (io, app) => {
       room = room + ''
       socket.join(room)
       socket.room = room
+      if(!loggers.room) {
+        loggers.room = makeLogger(room+'.jsonl')
+      }
     })
 
     socket.on('disconnect', () => {
       if(socket.room) {
         io.to(socket.room).emit('left room', socket.id)
+        loggers.room.info({ event: 'left room', data: socket.id })
         socket.leave(socket.room)
       }
     })
@@ -26,6 +49,7 @@ module.exports = (io, app) => {
       if(socket.room) {
         GazeData.sid = socket.id
         socket.to(socket.room).emit('gaze', GazeData);
+        loggers.room.info({ event: 'gaze', data: GazeData })
       }
     })
 
@@ -33,6 +57,7 @@ module.exports = (io, app) => {
       if(socket.room) {
         data.sid = socket.id
         socket.to(socket.room).emit('scroll', data);
+        loggers.room.info({ event: 'scroll', data })
       }
     })
 
@@ -40,6 +65,7 @@ module.exports = (io, app) => {
       if(socket.room) {
         MouseData.sid = socket.id
         socket.to(socket.room).emit('mouse', MouseData);
+        loggers.room.info({ event: 'mouse', data: MouseData })
       }
     })
 
